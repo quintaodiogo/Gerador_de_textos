62a8baf4adbe3ebca48b8ec02732c5d36bfb918d
Diogo <quintaodiogo@gmail.com>
2023-09-07 10:33:14 -0300

    foi adicionado header, footer e um botão de copiar o texto

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
-const btn = document.querySelector("#send")
-const clean = document.querySelector("#clear")
-const output = document.querySelector("#output")
+const btn = document.querySelector("#enviar");
+const clean = document.querySelector("#clear");
+const output = document.querySelector("#output");
+const copiar = document.querySelector("#copiar");
+let textoCopiado = "";
 
-btn.addEventListener("click", function(e){
-    e.preventDefault()
-    const name = document.querySelector("#name")
-    const valor_atual = document.querySelector("#valor_atual")
-    const valor_cupom = document.querySelector("#valor_cupom")
-    const cupom = document.querySelector("#cupom")
-    const link = document.querySelector("#link")
-    const soma = Number(valor_atual.value) - Number(valor_cupom.value)
-    let value = ''
-
-    if(cupom.value==""){
-        value =  `${'\u{1F6D2}'} <strong>${name.value}</strong><br>${'\u{1F4B8}'} Valor = <strong>R$ ${valor_atual.value}</strong> <br>${'\u{1F517}'} ${link.value}
-        <br>
-        <br>
-        ${'\u{2705}'}CUPONOMIA:<br>https://www.cuponomia.com.br/ref/0702d2e7d253<br>
-        ${'\u{2705}'}CANAL DO TELEGRAM:<br>https://t.me/+7J9eRe8OruM1YjUx`
-    } else{
-         value =  `${'\u{1F6D2}'} <strong>${name.value}</strong><br>
-        ${'\u{1F4B8}'} Valor = <strong>R$ ${soma}  (C/ cupom do vendedor)</strong> <br>
-        ${'\u{1F911}'} Cupom: ${cupom.value}<br>
-        ${'\u{1F517}'} ${link.value}<br>
-        <br>
-        ${'\u{2705}'}CUPONOMIA:<br>https://www.cuponomia.com.br/ref/0702d2e7d253
-        <br>
-    ${'\u{2705}'}CANAL DO TELEGRAM:<br>https://t.me/+7J9eRe8OruM1YjUx
-    `
-    }
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const name = document.querySelector("#name");
+  const valor_atual = document.querySelector("#valor_atual");
+  const valor_cupom = document.querySelector("#valor_cupom");
+  const cupom = document.querySelector("#cupom");
+  const link = document.querySelector("#link");
+  const soma = Number(valor_atual.value) - Number(valor_cupom.value);
+  if (isNaN(soma)) {
+    alert(
+      "Por favor, insira um valor numérico válido em 'Valor Atual' e 'Valor do Cupom'."
+    );
+    return;
+  }
+  if (cupom.value == "") {
+    textoCopiado = `${"\u{1F6D2}"} ${name.value}
+    ${"\u{1F4B8}"}Valor = R$ ${valor_atual.value}
+    ${"\u{1F517}"}${link.value}
     
-    output.innerHTML = value
-    name.value = ""
-    valor_atual.value = ""
-    valor_cupom.value = ""
-    cupom.value = ""
-    link.value = ""
-})
+    ${"\u{2705}"}CUPONOMIA: https://www.cuponomia.com.br/ref/0702d2e7d253
+    ${"\u{2705}"}CANAL DO TELEGRAM: https://telegram.org/`;
+  } else {
+    textoCopiado = `${"\u{1F6D2}"} ${name.value}
+    ${"\u{1F4B8}"}Valor = R$ ${soma}  (C/ cupom do vendedor)
+    ${"\u{1F911}"}Cupom: ${cupom.value}
+    ${"\u{1F517}"}${link.value}
+    
+    ${"\u{2705}"}CUPONOMIA: https://www.cuponomia.com.br/ref/0702d2e7d253
+    ${"\u{2705}"}CANAL DO TELEGRAM: https://telegram.org/`;
+  }
+  copiar.style.display = "block";
+  output.innerText = textoCopiado;
+  name.value = "";
+  valor_atual.value = "";
+  valor_cupom.value = "";
+  cupom.value = "";
+  link.value = "";
+});
 
-clean.addEventListener("click", function(a){
-    a.preventDefault()
-    const name = document.querySelector("#name")
-    const value =  ""
-    output.innerHTML = value
-})
+clean.addEventListener("click", function (a) {
+  a.preventDefault();
+  const name = document.querySelector("#name");
+  textoCopiado = "";
+  output.innerText = "";
+  copiar.style.display = "none";
+});
+
+copiar.addEventListener("click", function (b) {
+  b.preventDefault();
+  if (textoCopiado) {
+    const textarea = document.createElement("textarea");
+    textarea.value = textoCopiado;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert("Texto copiado com sucesso!");
+  } else {
+    alert("Nenhum texto para copiar.");
+  }
+});
